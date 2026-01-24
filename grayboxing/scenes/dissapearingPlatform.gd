2b6a8100257292ab84d0bad2985219bb4ca12535
BossIion <ulysse.schaff@gmail.com>
2026-01-24 12:05:31 -0500

    Stairs

--- grayboxing/scenes/dissapearingPlatform.gd
+++ grayboxing/scenes/dissapearingPlatform.gd
@@ -8,11 +8,24 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	pass
 
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
-		
-	pass # Replace with function body.
+		$Timer.start()
+		print("body deteced")
+		$Timer2.start()
+
+func _on_timer_timeout() -> void:
+
+	$"Platform 8"["visible"] = false
+	$"Platform 8"["use_collision"] = false
+	
+
+
+
+func _on_timer_2_timeout() -> void:
+	print("respawning")
+	$"Platform 8"["visible"] = true
+	$"Platform 8"["use_collision"] = true
