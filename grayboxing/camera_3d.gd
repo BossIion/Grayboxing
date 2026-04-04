68cee979bac1e96d6f87900cf486050ac78d4299
BossIion <ulysse.schaff@gmail.com>
2026-04-04 12:05:39 -0400

    Pinball pusher works

--- grayboxing/camera_3d.gd
+++ grayboxing/camera_3d.gd
@@ -1,11 +1,13 @@
-extends Area3D
+extends Node3D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	print($".".global_rotation)
+	#$".".rotate(Vector3.UP,$"../CSGSphere3D".global_rotation.x)
+	$".".global_rotation = Vector3(0, $"../CSGSphere3D".global_rotation.x, 0)
